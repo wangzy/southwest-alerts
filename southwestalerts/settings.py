5ff2f73b1a1269490c4b21dae73247a53730c491
Zhiyong Wang <zhiyong.wang@gmail.com>
2019-08-18 08:53:48 -0700

    cleanup

--- southwestalerts/settings.py
+++ southwestalerts/settings.py
@@ -11,12 +11,6 @@ class User:
         self.password = password
         self.headers = headers
 
-# Find all USERNAME# / PASSWORD# pairs and add the to the list of users to check
-#_index = 1
 users = []
-#while os.environ.get('USERNAME{}'.format(_index)):
-#    user = User(os.environ['USERNAME{}'.format(_index)], os.environ['PASSWORD{}'.format(_index)], None)
-#    users.append(user)
-#    _index += 1
 for k, v in json.load(open('accounts.json')).items():
     users.append(User(str(k),str(v),None))
